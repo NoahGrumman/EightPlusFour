0dbbd3638b10c87851d39a0b02a9ac8824c9bc84
Noah Grumman <ngrumman@sbcglobal.net>
2016-01-20 22:56:03 -0500

    added automatic scrolling on window resize

--- client/js/Main.js
+++ client/js/Main.js
@@ -15,6 +15,28 @@ if (Meteor.isClient) {
         $('.navbar').style("background:transparent;");
       }
     },false);
+
+    $(window).resize(function(){
+      var windowHeight = $(window).height();
+      var scrollPosition = $(document).scrollTop();
+      console.log("resizing");
+      console.log("windowHeight: " + windowHeight - 100);
+      console.log("scrollPosition: " + scrollPosition);
+      if (scrollPosition < windowHeight){
+        window.scrollTo(0,$("#intro").offset().top);
+      }
+      else if (scrollPosition < windowHeight*2){
+        window.scrollTo(0,$("#proposal").offset().top);
+      }
+      else if (scrollPosition < windowHeight*3){
+        window.scrollTo(0,$("#endorsements").offset().top);
+      }
+      else {
+        window.scrollTo(0,$("#vote").offset().top)
+      }
+    });
+
+
   });
 
   Template.vote.helpers({
